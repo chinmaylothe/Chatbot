2d1b8c36153f10ac24ddb1808cbe99258d198d2b
chinmaylothe <chinmaylothe@gmail.com>
2024-04-30 18:12:30 +0530

    feat:updated app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+from model import preprocess_data, answer_question
 
 app = Flask(__name__)
 
@@ -6,10 +7,18 @@ app = Flask(__name__)
 def upload_data():
     if request.method == 'POST':
         file = request.files['file']
-        # Save the uploaded file
+        
         file.save('data.txt')
         return 'File uploaded successfully'
     return render_template('upload.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/ask', methods=['POST'])
+def ask_question():
+    question = request.form['question']
+    with open('data.txt', 'r') as f:
+        data = f.read()
+    answer = answer_question(question, data)
+    return answer
